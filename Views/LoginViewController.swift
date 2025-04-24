46b76773d4dd253d8271e0ea46378c6edb3bca46
Dmitry Kuznetsov <dmkv2004@mail.ru>
2025-04-24 16:04:51 +0300

    feat: add view

--- Views/LoginViewController.swift
+++ Views/LoginViewController.swift
@@ -1,5 +1,101 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-    var viewModel: LoginViewModelProtocol!
+
+    private let viewModel: LoginViewModelProtocol
+        
+    init(viewModel: LoginViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private let usernameField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Username"
+        textField.borderStyle = .roundedRect
+        textField.autocapitalizationType = .none
+        return textField
+    }()
+
+    private let passwordField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Password"
+        textField.borderStyle = .roundedRect
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Login", for: .normal)
+        return button
+    }()
+
+    private let errorLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .red
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.numberOfLines = 0
+        label.isHidden = true
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setupActions()
+    }
+
+    private func setupUI() {
+        view.backgroundColor = .white
+        [usernameField, passwordField, loginButton, errorLabel].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+
+        NSLayoutConstraint.activate([
+            usernameField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            usernameField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            usernameField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+
+            passwordField.topAnchor.constraint(equalTo: usernameField.bottomAnchor, constant: 20),
+            passwordField.leadingAnchor.constraint(equalTo: usernameField.leadingAnchor),
+            passwordField.trailingAnchor.constraint(equalTo: usernameField.trailingAnchor),
+
+            loginButton.topAnchor.constraint(equalTo: passwordField.bottomAnchor, constant: 30),
+            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+
+            errorLabel.topAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: 10),
+            errorLabel.leadingAnchor.constraint(equalTo: usernameField.leadingAnchor),
+            errorLabel.trailingAnchor.constraint(equalTo: usernameField.trailingAnchor)
+        ])
+    }
+
+    private func setupActions() {
+        loginButton.addTarget(self, action: #selector(loginTapped), for: .touchUpInside)
+    }
+
+    @objc private func loginTapped() {
+        guard let username = usernameField.text, !username.isEmpty,
+              let password = passwordField.text, !password.isEmpty else {
+            showError("Please enter both username and password.")
+            return
+        }
+
+        if viewModel.login(username: username, password: password) {
+            errorLabel.isHidden = true
+            print("Logged in successfully!")
+        } else {
+            showError("Invalid credentials. Try again.")
+        }
+    }
+
+    private func showError(_ message: String) {
+        errorLabel.text = message
+        errorLabel.isHidden = false
+    }
 }
